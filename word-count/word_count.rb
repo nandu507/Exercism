d9887b066a999b363dcc66af29d3d2933a1431fd
nandu507 <nsankar507@gmail.com>
2016-10-04 18:13:00 +0530

    The last test case is to be complete

---
+++ word-count/word_count.rb
@@ -0,0 +1,16 @@
+ class Phrase
+   def initialize(phrase)
+     @phrases = phrase.downcase
+   end
+
+   def word_count
+     @phrases.gsub!(/[^0-9A-Za-z\b']/,' ')
+
+     @words = @phrases.split
+     count = Hash.new 0
+     @words.each do|word|
+       count[word] += 1
+     end
+     count
+   end
+end

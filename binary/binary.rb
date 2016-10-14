52a0289c4d5c02379d75506193646fb64a3588da
nandu507 <nsankar507@gmail.com>
2016-10-14 13:31:44 +0530

    binary to decimal conversion

---
+++ binary/binary.rb
@@ -0,0 +1,21 @@
+class Binary
+  def initialize(binary_num)
+    @binary_num = binary_num
+  end
+
+  def to_decimal
+    raise(ArgumentError) if (@binary_num) =~ /[2-9a-zA-z]/
+    temp_array = @binary_num.split ""
+    count = 0
+    decimal_num = 0
+    until temp_array.empty?
+      binary_decimal = temp_array.pop.to_i
+      decimal_num += binary_decimal * 2**count
+      count += 1
+    end
+    decimal_num
+  end
+end
+module BookKeeping
+  VERSION = 2
+end

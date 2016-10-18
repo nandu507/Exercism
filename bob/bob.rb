295013bc0ad13c7152f839c20d875c08d20a6687
nandu507 <nsankar507@gmail.com>
2016-10-18 17:51:50 +0530

    complete 12 test of 20 test case

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,5 +1,19 @@
 class Bob
   def hey(input)
-    
+    bob = {:yell => 'Whoa, chill out!',
+           :question => 'Sure.',
+           :address_him => 'Fine. Be that way!',
+           :anything_else => 'Whatever.'  }
+    talk = input.chomp
+    case(talk)
+    when talk =~ /\s/
+      bob[:address_him]
+    when talk[-1] == '?'
+      bob[:question]
+    when talk.upcase
+      bob[:yell]
+    else
+      bob[:anything_else]
+    end
   end
 end

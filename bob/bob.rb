5d277978a70711a36f922dfbb908538257e3b966
nandu507 <nsankar507@gmail.com>
2016-10-28 11:49:09 +0530

    Change the case statement to if statement

--- bob/bob.rb
+++ bob/bob.rb
@@ -5,15 +5,11 @@ class Bob
            :address_him => 'Fine. Be that way!',
            :anything_else => 'Whatever.'  }
     talk = input.chomp
-    case(talk)
-    when talk.empty?
-      bob[:address_him]
-    when talk[-1] == '?'
-      bob[:question]
-    when talk.upcase
-      bob[:yell]
-    else
-      bob[:anything_else]
-    end
+    return bob[:yell] if talk == talk.upcase && talk.match(/[A-Z]+/)
+    return bob[:question] if talk[-1] == '?'
+    return bob[:address_him] if talk.empty? || talk.match(/^\s+$/)
+    bob[:anything_else]
+
+
   end
 end

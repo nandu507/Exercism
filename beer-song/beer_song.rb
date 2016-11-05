3337d84cfb217669159a0d508a06c9210ed94798
nandu507 <nsankar507@gmail.com>
2016-11-05 07:26:27 +0530

    initial stage- one def is written

---
+++ beer-song/beer_song.rb
@@ -0,0 +1,13 @@
+class BeerSong
+  def version(number1 = 0, number2 = 0)
+    if number2 > 0
+      limit = (number2 - number1) + 1
+    end
+    while limit >= 0
+      if number = 1
+        number =  
+
+    end
+
+  end
+end

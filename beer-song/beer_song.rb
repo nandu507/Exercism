fa75060f977e8c1eced694a42c2a82669fa0e610
nandu507 <nsankar507@gmail.com>
2016-11-06 23:57:55 +0530

    add funtions

--- beer-song/beer_song.rb
+++ beer-song/beer_song.rb
@@ -1,13 +1,56 @@
 class BeerSong
-  def version(number1 = 0, number2 = 0)
-    if number2 > 0
-      limit = (number2 - number1) + 1
-    end
-    while limit >= 0
-      if number = 1
-        number =  
+  def initialize(number1 = 0,number2 = 0)
+    @number1 = number1
+    @number2 = number2
+  end
 
+  def version
+    limit = 0
+    if @number2 > 0 && @number1 > 0
+      limit = (@number2 - @number1) + 1
+    elsif number1 > 0
+      limit = 1
+    else
+      limit = 0
     end
 
+    if @number1 == 0
+      @number1 = 99
+    end
+
+    if limit = 0
+      limit = 100
+    end
+
+    while limit > 0
+      to_s
+      limit -= 1
+    end
+  end
+
+  def to_s
+    "#{first_line(@number1)}\n"
+    "#{secound_line(@number1)}\n"
+  end
+
+  def first_line(@number1)
+    "#{bottles(@number1)} bottles of beer on the wall, #{bottles(@number1)} bottles of beer."
+  end
+
+  def secound_line(@number1)
+    "Take one down and pass it around, #{bottles(@number1 - 1)} bottle of beer on the wall."
+  end
+
+  def bottles(remaining)
+    case remaining
+    when -1
+      '99 bottle'
+    when 0
+      'no more bottle'
+    when 1
+      '1 bottle'
+    else
+      '{remaining}bottle'
+    end
   end
 end

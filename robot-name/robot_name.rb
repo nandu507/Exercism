46e6ca2fcb661941d71379c240034f9d7f2f3565
nandu507 <nsankar507@gmail.com>
2016-11-12 22:30:04 +0530

    give name to robot

---
+++ robot-name/robot_name.rb
@@ -0,0 +1,14 @@
+class Robot
+
+  def name
+    alphabet = ('A'..'Z').to_a.sample(2)
+    alphabet << (0..9).to_a.sample(3)
+    alphabet.join
+    name_storage = Array.new
+    if 
+  end
+
+  def reset
+    alphabet = ""
+  end
+end

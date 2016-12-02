589929af798f2590b9558df92a8dc459034452f3
nandu507 <nsankar507@gmail.com>
2016-12-02 23:14:24 +0530

    test completed

--- nucleotide-count/nucleotide_count_test.rb
+++ nucleotide-count/nucleotide_count_test.rb
@@ -5,6 +5,7 @@ require_relative 'nucleotide_count'
 
 class NucleotideTest < Minitest::Test
   def test_empty_dna_strand_has_no_adenosine
+    #skip
     assert_equal 0, Nucleotide.from_dna('').count('A')
   end
 
@@ -27,19 +28,19 @@ class NucleotideTest < Minitest::Test
   end
 
   def test_empty_dna_strand_has_no_nucleotides
-    skip
+    #skip
     expected = { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
     assert_equal expected, Nucleotide.from_dna('').histogram
   end
 
   def test_repetitive_sequence_has_only_guanosine
-    skip
+    #skip
     expected = { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 8 }
     assert_equal expected, Nucleotide.from_dna('GGGGGGGG').histogram
   end
 
   def test_counts_all_nucleotides
-    #skip
+  #  skip
     s = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
     dna = Nucleotide.from_dna(s)
     expected = { 'A' => 20, 'T' => 21, 'G' => 17, 'C' => 12 }
@@ -47,7 +48,7 @@ class NucleotideTest < Minitest::Test
   end
 
   def test_validates_dna
-    skip
+    #skip
     assert_raises ArgumentError do
       Nucleotide.from_dna('JOHNNYAPPLESEED')
     end
